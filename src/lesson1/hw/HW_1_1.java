7190753d73879d61c5665f1e557bc24cfe52660c
Yahor Labanau <yahorlabanau@gmail.com>
2023-08-10 15:56:57 +0300

    1st Java Lesson with Homework

---
+++ src/lesson1/hw/HW_1_1.java
@@ -0,0 +1,21 @@
+package lesson1.hw;
+
+// Task Description:
+/*
+Написать программу которая будет печатать/выводить следующий текст:
+
+3
+2
+1
+Поехали!!!
+ */
+
+
+public class HW_1_1 {
+    public static void main(String[] args) {
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Поехали!!!");
+    }
+}
---
+++ src/lesson1/lecture/LR_1.java
@@ -0,0 +1,7 @@
+package lesson1.lecture;
+
+public class LR_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
