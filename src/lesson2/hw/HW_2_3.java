972819ebf3110a5828f6e7a68578ffec59f34e4d
Yahor Labanau <yahorlabanau@gmail.com>
2023-08-14 06:19:29 +0300

    lesson2.hw: another solution to "smile task"

--- src/lesson2/hw/HW_2_3.java
+++ src/lesson2/hw/HW_2_3.java
@@ -1,8 +1,15 @@
 package lesson2.hw;
 
+// Task Description:
+/*
+Вывести/напечатать смайлик (не :),
+а настоящий смайлик одним символом).
+*/
+
 public class HW_2_3 {
     public static void main(String[] args) {
         System.out.println("\uD83D\uDE00");
         System.out.println("\uD83D\uDC7D");
+        System.out.println("😀");
     }
 }
