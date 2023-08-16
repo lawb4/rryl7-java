c45b6bd2716a03476a6def7047101b5511508bf5
Yahor Labanau <yahorlabanau@gmail.com>
2023-08-16 06:34:45 +0300

    hw_2_2 revision with comments

--- src/lesson2/hw/HW_2_2.java
+++ src/lesson2/hw/HW_2_2.java
@@ -15,18 +15,23 @@ package lesson2.hw;
 public class HW_2_2 {
     public static void main(String[] args) {
         int coins = 1000;
-        int crewSize = 21;
+        int crewSize = 19;
+        int crewSizeWithCaptain = crewSize + 1;
 
+        // owner gets half the loot
         int coinsToOwner = coins / 2;
+        // captain gets half the loot remaining from owner
         int coinsToCaptain = (coins - coinsToOwner) / 2;
+        // each crewMember + the captain gets the rest of the money proportionally
         int coinsToEachCrewMember =
-                (coins - coinsToOwner - coinsToCaptain) / crewSize;
+                (coins - coinsToOwner - coinsToCaptain) / crewSizeWithCaptain;
 
-        int coinsDistributed = coinsToOwner + coinsToCaptain + (coinsToEachCrewMember * crewSize);
+        int coinsDistributed = coinsToOwner + coinsToCaptain + (coinsToEachCrewMember * crewSizeWithCaptain);
 
         System.out.println("coins to owner: " + coinsToOwner);
         System.out.println("coins to captain: " + coinsToCaptain);
         System.out.println("coins to each crew member: " + coinsToEachCrewMember);
+        System.out.println("coins to all the  crew member: " + coinsToEachCrewMember * crewSizeWithCaptain);
         System.out.println("coins distributed: " + coinsDistributed);
         System.out.println("coins leftover: " + (coins - coinsDistributed));
 
