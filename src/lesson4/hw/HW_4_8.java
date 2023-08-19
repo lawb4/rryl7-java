3030e15a3128026c06c9b77026b73f990b60538f
Yahor Labanau <yahorlabanau@gmail.com>
2023-08-19 08:06:49 +0300

    hw_4_8

---
+++ src/lesson4/hw/HW_4_8.java
@@ -0,0 +1,35 @@
+package lesson4.hw;
+
+/*
+Задача №8
+
+Дан массив произвольных целых чисел.
+    Необходимо подсчитать количество положительных и отрицательных чисел в этом массиве.
+*/
+
+public class HW_4_8 {
+    public static void main(String[] args) {
+
+        int[] arr = {-45, -65, -34, 65, 0, 1, 78, 143, -69, -65};
+
+
+        int countPositive = 0;
+        int countNegative = 0;
+        int countZeroes = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                countPositive++;
+            } else if (arr[i] < 0) {
+                countNegative++;
+            } else {
+                countZeroes++;
+            }
+        }
+        System.out.println("Counter of All values in Array = " + arr.length);
+        System.out.println("Counter of Posivites = " + countPositive);
+        System.out.println("Counter of Negatives = " + countNegative);
+        System.out.println("Counter of Zeroes = " + countZeroes);
+
+    }
+}
