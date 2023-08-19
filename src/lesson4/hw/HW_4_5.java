158ccfb6977c0cad93a59341193eeca15bd81771
Yahor Labanau <yahorlabanau@gmail.com>
2023-08-19 08:06:23 +0300

    hw_4_5

---
+++ src/lesson4/hw/HW_4_5.java
@@ -0,0 +1,24 @@
+package lesson4.hw;
+
+/*
+Задача №5
+
+Дан массив:
+    int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+    необходимо вывести максимальное значение массива.
+*/
+
+public class HW_4_5 {
+    public static void main(String[] args) {
+
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
