8cc9d7c706e05e20886b4dfb90fd105eceefb0bf
Yahor Labanau <yahorlabanau@gmail.com>
2023-08-19 08:06:05 +0300

    hw_4_1

---
+++ src/lesson4/hw/HW_4_1.java
@@ -0,0 +1,16 @@
+package lesson4.hw;
+
+/*Задача №1
+
+Необходимо вывести числа от 0 до 15.
+*/
+
+
+public class HW_4_1 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(i);
+        }
+    }
+}
