63d6bef43c92c54ce2cffc3c25a3b72931e88c68
Yahor Labanau <yahorlabanau@gmail.com>
2023-08-19 08:06:29 +0300

    hw_4_6

---
+++ src/lesson4/hw/HW_4_6.java
@@ -0,0 +1,24 @@
+package lesson4.hw;
+
+/*
+Задача №6
+
+Дан массив:
+    int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+    необходимо вывести минимальное значение массива.
+*/
+
+public class HW_4_6 {
+    public static void main(String[] args) {
+
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
