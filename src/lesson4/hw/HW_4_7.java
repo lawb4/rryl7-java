ff7847b46634be232e29a61431040fea7c3d77e9
Yahor Labanau <yahorlabanau@gmail.com>
2023-08-19 08:06:34 +0300

    hw_4_7

---
+++ src/lesson4/hw/HW_4_7.java
@@ -0,0 +1,23 @@
+package lesson4.hw;
+
+/*
+Задача №7
+
+Дан массив:
+    int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    необходимо вывести среднее арифметическое всех значений массива.
+
+*/
+
+public class HW_4_7 {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println(sum / array.length);
+    }
+}
