a2b1c17f485cc512a28ff19d8eb3cf3960c3616c
Yahor Labanau <yahorlabanau@gmail.com>
2023-09-04 07:04:46 +0300

    hw_7_1 : refactoring

--- src/lesson7/hw7_1/Person.java
+++ src/lesson7/hw7_1/Person.java
@@ -3,16 +3,19 @@ package lesson7.hw7_1;
 public class Person {
     String name;
     int age;
+
     boolean sex; // true = male, false = female
 
     public String getName() {
         if (sex) {
             return "Mr. " + name;
-        } else
+        } else {
             return "Mrs. " + name;
+        }
 
         /*
         2nd way with ternary operator
+
         String prefix = sex ? "Mr" : "Mrs";
         return prefix + ". " + name;
         */
