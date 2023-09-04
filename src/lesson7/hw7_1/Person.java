c8c3e17f54bab278ebeec81a243ea8ad0e991f5d
Yahor Labanau <yahorlabanau@gmail.com>
2023-09-04 06:53:09 +0300

    hw_7_1 : review

--- src/lesson7/hw7_1/Person.java
+++ src/lesson7/hw7_1/Person.java
@@ -3,12 +3,18 @@ package lesson7.hw7_1;
 public class Person {
     String name;
     int age;
-    boolean sex;
+    boolean sex; // true = male, false = female
 
     public String getName() {
         if (sex) {
             return "Mr. " + name;
         } else
             return "Mrs. " + name;
+
+        /*
+        2nd way with ternary operator
+        String prefix = sex ? "Mr" : "Mrs";
+        return prefix + ". " + name;
+        */
     }
 }
